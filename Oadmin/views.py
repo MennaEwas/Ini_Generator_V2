3d62bc5e0b8c41a1da251f11b83f42a624b6b333
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2023-06-04 16:15:46 +0300

    Add files via upload

--- Oadmin/views.py
+++ Oadmin/views.py
@@ -1,32 +1,37 @@
-import configparser
-from django.http import HttpResponse
 import yaml
-import json
-
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import FilesUpload
 
 class admin_view_selectg(LoginRequiredMixin, View):
     template_name = 'Oadmin/selectg.html'
 
     def get(self, request):
-        return render(request, self.template_name, {'yaml_content': ''})
+        files = FilesUpload.objects.values_list('id','file')
+        return render(request, self.template_name, {'files': files})
 
     def post(self, request):
-        yaml_content = request.POST.get('yaml_content')
-        yaml_data = yaml.safe_load(yaml_content) #convert into yaml object 
-        
-        with open('Myyaml.yaml', 'w') as yaml_file:
-            yaml.dump(yaml_data, yaml_file)
+        file2 = request.FILES["file"]
+        document = FilesUpload.objects.create(file = file2)
+        document.save()
+        return HttpResponse("file uploaded")
+ 
 
-        return HttpResponse("Finished!")
+class show_all(LoginRequiredMixin, View):
+    template_name = 'Oadmin/show_all.html'
+
+    def get(self, request):
+        files_names = request.GET.get('file_content', '')  # Retrieve the file content from the query parameter
+        return render(request, self.template_name, {'content': files_names})
 
 
+class show(LoginRequiredMixin, View):
+    template_name = 'Oadmin/show.html'
 
+    def get(self, request):
+        file_content = request.GET.get('file_content', '')  # Retrieve the file content from the query parameter
+        return render(request, self.template_name, {'content': file_content})
 
-
-
-
-# BSP Params
-# PCMLawSelect
